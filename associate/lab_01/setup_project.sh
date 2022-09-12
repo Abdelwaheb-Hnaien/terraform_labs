9bff5997c80ba35fc158fe1de40a423a4de06737
Mrx <abdelwaheb.hnaien@gmail.com>
2022-09-12 16:42:03 +0200

    update script

--- associate/lab_01/setup_project.sh
+++ associate/lab_01/setup_project.sh
@@ -2,4 +2,4 @@ set -eEuo pipefail
 set +x
 gcloud config set project $1
 find associate/lab_01/iac -type f -name "provider.tf" -exec sed -i "s/project =.*/project = \"$1\"/g" {} +
-find sociate/lab_01/iac -type f -name "provider.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
+find associate/lab_01/iac -type f -name "provider.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
