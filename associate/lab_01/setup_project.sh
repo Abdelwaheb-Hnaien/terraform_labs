9a3b93d5b4375fdeaead0ff46f6b0d7ee8737f99
Mrx <abdelwaheb.hnaien@gmail.com>
2022-09-12 15:52:52 +0200

    update iac

--- associate/lab_01/setup_project.sh
+++ associate/lab_01/setup_project.sh
@@ -1,3 +1,5 @@
+set -eEuo pipefail
+set +x
 gcloud config set project $1
-find . -type f -name "provider.tf" -exec sed -i "s/project =.*/project = \"$1\"/g" {} +
-find . -type f -name "provider.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
+find associate/lab_01/iac -type f -name "provider.tf" -exec sed -i "s/project =.*/project = \"$1\"/g" {} +
+find sociate/lab_01/iac -type f -name "provider.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
